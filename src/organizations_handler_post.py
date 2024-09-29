ff8347e2d718eeb7c65bdc25721c7712e95e0a5a
Delvin Hall <dlvhall@gmail.com>
2024-09-29 16:28:29 -0500

    fix for create organization endpoint

--- src/organizations_handler_post.py
+++ src/organizations_handler_post.py
@@ -2,14 +2,13 @@ import uuid
 from datetime import datetime, timezone
 import json
 from src.organizations import create_organization
-from src.authorization import create_cognito_group, add_user_to_group, update_user_org_id, create_admin_user
+from src.authorization import create_cognito_group, create_admin_user
 from src.http_response import create_response
 
 def handler(event, _):
     body = json.loads(event['body'])
     org_id = str(uuid.uuid4())
     org_name = body.get('name')
-    username = str(uuid.uuid4())
     email = body.get('email')
 
     organization = {
@@ -29,8 +28,6 @@ def handler(event, _):
         create_organization(organization)
         create_cognito_group(org_id, org_name)
         create_admin_user(email, org_id)
-        add_user_to_group(username, org_id)
-        update_user_org_id(username, org_id)
-        return create_response(200, f"Organization: '{org_name}'' created successfully. Admin: {email}")
+        return create_response(200, f"Organization: '{org_name}' created successfully. Admin: {email}")
     except Exception as e:
         return create_response(500, f'Error creating organization: {e}')
