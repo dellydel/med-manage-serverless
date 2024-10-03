7069be784cbda92ac544fc016e0e8915c4ba0dd7
Delvin Hall <dlvhall@gmail.com>
2024-10-03 13:07:03 -0500

    fix for missing file for new user creation

--- src/new_user_handler_post.py
+++ src/new_user_handler_post.py
@@ -2,6 +2,7 @@ import json
 from src.authorization import signup_new_user
 
 def handler(event, _):
+    # TODO: get the org_id from the event instead of the body
     body = json.loads(event['body'])
     email = body.get('email')
     org_id = body.get('org_id')
