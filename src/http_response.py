8001cff19b0a525a07991c48dedff6f777ac3d10
Delvin Hall <dlvhall@gmail.com>
2024-10-10 15:29:03 -0500

    fix for CORS

--- src/http_response.py
+++ src/http_response.py
@@ -5,10 +5,9 @@ def create_response(status_code, body):
     "statusCode": status_code,
     "headers": {
       "Content-Type": "application/json",
+      "Access-Control-Allow-Headers": "Content-Type",
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "OPTIONS,POST,GET,DELETE",
     },
     "body": body if isinstance(body, str) else json.dumps(body)
   }
-
-
