8d4b5f162181ebaec1a2d71cc314ddeaf8b79cda
Delvin Hall <dlvhall@gmail.com>
2024-10-17 17:50:40 -0500

    Fix for id token usage

--- src/utils/token.py
+++ src/utils/token.py
@@ -3,7 +3,8 @@ import jwt
 def get_token_from_event(event):
     headers = event['headers']
     token = headers.get("Authorization")
+    id_token = token.split(" ")[1]
     if not token:
         return None
-    decodedToken = jwt.decode(token, algorithms=["RS256"], options={"verify_signature": False})
+    decodedToken = jwt.decode(id_token, algorithms=["RS256"], options={"verify_signature": False})
     return decodedToken
